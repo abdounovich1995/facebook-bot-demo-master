50f6d7a0a96b2a2b674be3b44c57470ae819a90f
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-22 10:56:52 +0100

    back

--- templates/sendMessage.js
+++ templates/sendMessage.js
@@ -1,24 +1,24 @@
 const request = require('request');
-const senderAction = require('../templates/senderAction');
-const sendMessage = require('../templates/sendMessage');
-const sendGenericTemplate = require('../templates/sendGenericTemplate');
 
-module.exports = function processMessage(event) {
-    if (!event.message || !event.message.text) {
-        // If the message or text property is missing, don't process it further.
-        return;
-    }
-
-    const userMessage = event.message.text.toLowerCase(); // Convert the message to lowercase for case-insensitive matching.
-    const senderID = event.sender.id;
-    console.log("Received message from senderId: " + senderID);
-    console.log("Message is: " + JSON.stringify(event.message));
-
-    if (userMessage === "hello") {
-        // Send a "Hi" message in response to "Hello"
-        sendMessage(senderID, "Hi");
-    } else {
-        // Send an error message for other input
-        sendMessage(senderID, "I'm sorry, I don't under   stand that. Please try again.");
-    }
+module.exports = function sendMessage(recipientId, message){
+    return new Promise(function(resolve, reject) {
+        request({
+            url: "https://graph.facebook.com/v2.6/me/messages",
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN
+            },
+            method: "POST",
+            json: {
+                recipient: {id: recipientId},
+                message: message,
+            }
+        }, function(error, response, body) {
+            if (error) {
+                console.log("Error sending message: " + response.error);
+                reject(response.error);
+            } else {
+                resolve(body);
+            }
+        });
+    })
 }
