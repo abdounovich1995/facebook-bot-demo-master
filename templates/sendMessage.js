9467d251c94c96819980d129080f8d8fda121768
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-22 10:49:24 +0100

    sss

--- templates/sendMessage.js
+++ templates/sendMessage.js
@@ -4,20 +4,21 @@ const sendMessage = require('../templates/sendMessage');
 const sendGenericTemplate = require('../templates/sendGenericTemplate');
 
 module.exports = function processMessage(event) {
-    if (!event.message.is_echo) {
-        const message = event.message;
-        const senderID = event.sender.id;
-        console.log("Received message from senderId: " + senderID);
-        console.log("Message is: " + JSON.stringify(message));
-        if (message.text) {
-            const userMessage = message.text.toLowerCase(); // Convert the message to lowercase for case-insensitive matching.
-            if (userMessage === "hello") {
-                // Send a "Hi" message in response to "Hello"
-                sendMessage(senderID, "Hi");
-            } else {
-                // Send an error message for other input
-                sendMessage(senderID, "I'm sorry, I don't understand that. Please try again.");
-            }
-        }
+    if (!event.message || !event.message.text) {
+        // If the message or text property is missing, don't process it further.
+        return;
+    }
+
+    const userMessage = event.message.text.toLowerCase(); // Convert the message to lowercase for case-insensitive matching.
+    const senderID = event.sender.id;
+    console.log("Received message from senderId: " + senderID);
+    console.log("Message is: " + JSON.stringify(event.message));
+
+    if (userMessage === "hello") {
+        // Send a "Hi" message in response to "Hello"
+        sendMessage(senderID, "Hi");
+    } else {
+        // Send an error message for other input
+        sendMessage(senderID, "I'm sorry, I don't understand that. Please try again.");
     }
 }
