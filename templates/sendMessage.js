10ae483f7d95150dc7031e9ca821ddec1e16e218
abdounovich1995 <89597196+abdounovich1995@users.noreply.github.com>
2023-10-22 11:34:13 +0100

    delete

--- templates/sendMessage.js
+++ templates/sendMessage.js
@@ -1,26 +1,24 @@
-const request = require('request-promise');
-
 module.exports = function sendMessage(recipientId, message) {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
         console.log("Sending message to recipient: " + recipientId);
         request({
-            uri: "https://graph.facebook.com/v2.6/me/messages",
+            url: "https://graph.facebook.com/v2.6/me/messages",
             qs: {
-                access_token: process.env.PAGE_ACCESS_TOKEN,
+                access_token: process.env.PAGE_ACCESS_TOKEN
             },
             method: "POST",
             json: {
                 recipient: { id: recipientId },
                 message: message,
-            },
-        })
-            .then((body) => {
-                console.log("Message sent successfully.");
-                resolve(body);
-            })
-            .catch((error) => {
+            }
+        }, function (error, response, body) {
+            if (error) {
                 console.log("Error sending message: " + error);
                 reject(error);
-            });
+            } else {
+                console.log("Message sent successfully.");
+                resolve(body);
+            }
+        });
     });
-};
+}
